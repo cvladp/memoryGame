875ec63a8bd918c413e8f54eac5bcdeed5a6a8bd
Panoiu Vlad <vlad.panoiu@reevotech.com>
2023-02-15 15:31:25 +0200

    Moved counter position

--- src/scenes/GridBackground.ts
+++ src/scenes/GridBackground.ts
@@ -53,9 +53,9 @@ export class GridBackground extends Container {
 
     private addCounters() {
         this._counter.x = this._background.x + 75;
-        this._counter.y = this._background.y + this._counter.height;
+        this._counter.y = this._background.y + this._counter.height/2;
         this._bestScoreCounter.x = this._counter.x + 25;
-        this._bestScoreCounter.y = this._counter.y + this._counter.height * 3;
+        this._bestScoreCounter.y = this._counter.y + this._bestScoreCounter.height * 2;
         this._background.addChild(this._counter);
         this._background.addChild(this._bestScoreCounter);
     }
